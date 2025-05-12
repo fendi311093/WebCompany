e979e7cea2de348d57b20d827bcd492c1b641c49
fendi311093 <fendi.toro@gmail.com>
2025-05-12 15:37:22 +0700

    Adding panel gallery

---
+++ app/Filament/Resources/GalleryResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GalleryResource\Pages;
+use App\Filament\Resources\GalleryResource\RelationManagers;
+use App\Models\Gallery;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GalleryResource extends Resource
+{
+    protected static ?string $model = Gallery::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+    protected static ?string $navigationLabel = 'Gallery';
+    protected static ?string $pluralLabel = 'List Gallery';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()->schema([
+                    Grid::make(2)->schema([
+                        TextInput::make('title')
+                            ->label('Title Photo'),
+                        Toggle::make('is_published')
+                            ->onIcon('heroicon-m-check-badge')
+                            ->onColor('success')
+                            ->offIcon('heroicon-m-x-circle')
+                            ->offColor('danger')
+                    ])
+                ])->inlineLabel(),
+                Section::make()->schema([
+                    FileUpload::make('photo')
+                        ->required()
+                        ->image()
+                        ->maxSize(11000)
+                        ->multiple()
+                        ->directory('Gallery'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateIcon('heroicon-m-photo')
+            ->emptyStateHeading('no photos found')
+            ->emptyStateDescription('There is no gallery photo data, please add data first');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGalleries::route('/'),
+            'create' => Pages\CreateGallery::route('/create'),
+            'edit' => Pages\EditGallery::route('/{record}/edit'),
+        ];
+    }
+}
