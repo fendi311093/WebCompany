14d9a71b6f2d4dad260d5f4191442ca420c4ad1c
fendi311093 <48820689+fendi311093@users.noreply.github.com>
2025-05-14 20:52:31 +0700

    perbaikin penyesuaian jarak antar content di home blade

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -138,123 +138,99 @@
     @endif
 
     <!-- Clients -->
-    <div class="bg-gray-600 shadow-md px-4 sm:px-6 lg:px-8 py-6 mt-1">
-        <div class="container mx-auto">
-            <div class="text-center mb-6">
-                <h2 class="text-3xl font-semibold text-white">Our Customers</h2>
-                <p class="text-white mb-4">Companies that have collaborated with us</p>
-            </div>
-            <div class="flex flex-wrap justify-center items-center gap-8 md:gap-12">
-                @if ($customers->count() > 0)
-                    @foreach ($customers as $customer)
-                        <img src="{{ url('storage/' . $customer->logo) }}" alt="{{ $customer->name_customer }}"
-                            class="h-10 md:h-14 w-auto mx-4 object-contain transition duration-300" />
-                    @endforeach
-                @else
-                    <div class="text-red-500">Tidak ada data customer!</div>
-                @endif
+    <div class="bg-slate-200">
+        <div class="max-w-screen-2xl px-4 xl:px-0 py-3 mx-auto">
+            <div class="container mx-auto">
+                <div class="text-center mb-6">
+                    <h2 class="text-3xl font-semibold text-white">Our Customers</h2>
+                </div>
+                <div class="flex flex-wrap justify-center items-center gap-8 md:gap-12">
+                    @if ($customers->count() > 0)
+                        @foreach ($customers as $customer)
+                            <img src="{{ url('storage/' . $customer->logo) }}" alt="{{ $customer->name_customer }}"
+                                class="h-10 md:h-14 w-auto mx-4 object-contain transition duration-300" />
+                        @endforeach
+                    @else
+                        <div class="text-red-500">Tidak ada data customer!</div>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
     <!-- End Clients -->
 
-    <!-- Card Blog -->
-    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <!-- Title -->
-        <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Insights</h2>
-            <p class="mt-1 text-gray-600 dark:text-neutral-400">Stay in the know with insights from industry experts.
-            </p>
-        </div>
-        <!-- End Title -->
+    <!-- Stats -->
+    <div class="bg-neutral-900">
+        <div class="max-w-screen-2xl px-4 xl:px-0 py-6 mx-auto">
+            <div class="border border-neutral-800 rounded-xl">
+                <div class="p-4 lg:p-8 bg-gradient-to-bl from-neutral-800 via-neutral-900 to-neutral-950 rounded-xl">
+                    <div class="grid grid-cols-1 sm:grid-cols-3 items-center gap-y-20 gap-x-12">
+                        <!-- Stats -->
+                        <div
+                            class="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:w-px before:h-20 before:bg-neutral-800 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                            <svg class="shrink-0 size-6 sm:size-8 text-[#ff0] mx-auto"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round">
+                                <path d="m11 17 2 2a1 1 0 1 0 3-3" />
+                                <path
+                                    d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4" />
+                                <path d="m21 3 1 11h-2" />
+                                <path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3" />
+                                <path d="M3 4h8" />
+                            </svg>
+                            <div class="mt-3 sm:mt-5">
+                                <h3 class="text-lg sm:text-3xl font-semibold text-white">2,000+</h3>
+                                <p class="mt-1 text-sm sm:text-base text-neutral-400">Preline partners</p>
+                            </div>
+                        </div>
+                        <!-- End Stats -->
 
-        <!-- Grid -->
-        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <!-- Card -->
-            <a class="group flex flex-col focus:outline-hidden" href="#">
-                <div class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
-                    <img class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
-                        src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
-                        alt="Blog Image">
-                    <span
-                        class="absolute top-0 end-0 rounded-se-xl rounded-es-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-neutral-900">
-                        Sponsored
-                    </span>
-                </div>
+                        <!-- Stats -->
+                        <div
+                            class="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:w-px before:h-20 before:bg-neutral-800 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                            <div class="flex justify-center items-center -space-x-5">
+                                <img class="relative z-2 shrink-0 size-8 rounded-full border-3 border-neutral-800"
+                                    src="https://images.unsplash.com/photo-1601935111741-ae98b2b230b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                                    alt="Avatar">
+                                <img class="relative z-1 shrink-0 size-8 rounded-full border-3 border-neutral-800 -mt-7"
+                                    src="https://images.unsplash.com/photo-1570654639102-bdd95efeca7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                                    alt="Avatar">
+                                <img class="relative shrink-0 size-8 rounded-full border-3 border-neutral-800"
+                                    src="https://images.unsplash.com/photo-1679412330254-90cb240038c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=320&h=320&q=80"
+                                    alt="Avatar">
+                            </div>
+                            <div class="mt-3 sm:mt-5">
+                                <h3 class="text-lg sm:text-3xl font-semibold text-white">85%</h3>
+                                <p class="mt-1 text-sm sm:text-base text-neutral-400">Happy customers</p>
+                            </div>
+                        </div>
+                        <!-- End Stats -->
 
-                <div class="mt-7">
-                    <h3
-                        class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                        Studio by Preline
-                    </h3>
-                    <p class="mt-3 text-gray-800 dark:text-neutral-200">
-                        Produce professional, reliable streams easily leveraging Preline's innovative broadcast studio
-                    </p>
-                    <p
-                        class="mt-5 inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium dark:text-blue-500">
-                        Read more
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path d="m9 18 6-6-6-6" />
-                        </svg>
-                    </p>
-                </div>
-            </a>
-            <!-- End Card -->
-
-            <!-- Card -->
-            <a class="group flex flex-col focus:outline-hidden" href="#">
-                <div class="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
-                    <img class="size-full absolute top-0 start-0 object-cover group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
-                        src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
-                        alt="Blog Image">
-                </div>
-
-                <div class="mt-7">
-                    <h3
-                        class="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-neutral-300 dark:group-hover:text-white">
-                        Onsite
-                    </h3>
-                    <p class="mt-3 text-gray-800 dark:text-neutral-200">
-                        Optimize your in-person experience with best-in-class capabilities like badge printing and lead
-                        retrieval
-                    </p>
-                    <p
-                        class="mt-5 inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium dark:text-blue-500">
-                        Read more
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="m9 18 6-6-6-6" />
-                        </svg>
-                    </p>
-                </div>
-            </a>
-            <!-- End Card -->
-
-            <!-- Card -->
-            <a class="group relative flex flex-col w-full min-h-60 bg-[url('https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80')] bg-center bg-cover rounded-xl hover:shadow-lg focus:outline-hidden focus:shadow-lg transition"
-                href="#">
-                <div class="flex-auto p-4 md:p-6">
-                    <h3 class="text-xl text-white/90 group-hover:text-white"><span class="font-bold">Preline</span>
-                        Press publishes books about economic and technological advancement.</h3>
-                </div>
-                <div class="pt-0 p-4 md:p-6">
-                    <div
-                        class="inline-flex items-center gap-2 text-sm font-medium text-white group-hover:text-white/70 group-focus:text-white/70">
-                        Visit the site
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="m9 18 6-6-6-6" />
-                        </svg>
+                        <!-- Stats -->
+                        <div
+                            class="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:w-px before:h-20 before:bg-neutral-800 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                            <svg class="shrink-0 size-6 sm:size-8 text-[#ff0] mx-auto"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round">
+                                <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
+                                <path
+                                    d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
+                                <path d="m2 16 6 6" />
+                                <circle cx="16" cy="9" r="2.9" />
+                                <circle cx="6" cy="5" r="3" />
+                            </svg>
+                            <div class="mt-3 sm:mt-5">
+                                <h3 class="text-lg sm:text-3xl font-semibold text-white">$55M+</h3>
+                                <p class="mt-1 text-sm sm:text-base text-neutral-400">Ads managed yearly</p>
+                            </div>
+                        </div>
+                        <!-- End Stats -->
                     </div>
                 </div>
-            </a>
-            <!-- End Card -->
+            </div>
         </div>
-        <!-- End Grid -->
     </div>
-    <!-- End Card Blog -->
+    <!-- End Stats -->
 </div>
