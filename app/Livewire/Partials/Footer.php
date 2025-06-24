0cf50e7fcbdaef2d0397b49e4f7b54f6a2f80b4b
fendi311093 <48820689+fendi311093@users.noreply.github.com>
2025-06-24 20:25:36 +0700

    feat: tambahkan data profil perusahaan ke footer dan perbarui tampilan logo serta nama perusahaan di footer.blade.php untuk meningkatkan konsistensi informasi

--- app/Livewire/Partials/Footer.php
+++ app/Livewire/Partials/Footer.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire\Partials;
 
+use App\Models\Profil;
 use Livewire\Component;
 
 class Footer extends Component
 {
+    public $profilCompany;
+
+    public function mount()
+    {
+        $this->profilCompany = Profil::select('logo', 'name_company')->first();
+    }
+
     public function render()
     {
-        return view('livewire.partials.footer');
+        return view('livewire.partials.footer', [
+            'profilCompany' => $this->profilCompany
+        ]);
     }
 }
